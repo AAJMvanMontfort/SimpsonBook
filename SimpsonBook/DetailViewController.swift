5425ed3dadd5637c37566ba65e8f5753a16c50b5
AAJMvanMontfort <lout@me.com>
2020-07-14 22:53:32 +0200

    First setup

---
+++ SimpsonBook/DetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailViewController.swift
+//  SimpsonBook
+//
+//  Created by AAJM van Montfort on 14/07/2020.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var ivSimpson: UIImageView!
+    @IBOutlet weak var lblSimpsonName: UILabel!
+    @IBOutlet weak var lblSimpsonJob: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tvSimpson: UITableView!
+    
+    let homer = Simpsons(nameOfSimpson: "Homer Simpson", imageOfSimpson: UIImage(named: "Homer Simpson")!, jobOfSimpson: "Nuclear Safety Inspector")
+    let abraham = Simpsons(nameOfSimpson: "Abraham Simpson", imageOfSimpson: UIImage(named: "Abraham Simpson")!, jobOfSimpson: "NSprawl-Mart greeter")
+    let bart = Simpsons(nameOfSimpson: "Bart Simpson", imageOfSimpson: UIImage(named: "Bart Simpson")!, jobOfSimpson: "Student")
+    let herbert = Simpsons(nameOfSimpson: "Herbert Powell", imageOfSimpson: UIImage(named: "Herb Powell")!, jobOfSimpson: "Invemtor")
+    let lisa = Simpsons(nameOfSimpson: "Lisa Simpson", imageOfSimpson: UIImage(named: "Lisa Simpson")!, jobOfSimpson: "Student")
+    let maggie = Simpsons(nameOfSimpson: "Maggie Simpson", imageOfSimpson: UIImage(named: "Maggie Simpson")!, jobOfSimpson: "Baby")
+    let marge = Simpsons(nameOfSimpson: "Marge Simpson", imageOfSimpson: UIImage(named: "Marge Simpson")!, jobOfSimpson: "Housewife")
+    let montgomery = Simpsons(nameOfSimpson: "Montgomery Burns", imageOfSimpson: UIImage(named: "Montgomery Burns")!, jobOfSimpson: "Owner Nuclear Power plant")
+    
+    var simpsonArray = [Simpsons]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tvSimpson.delegate = self
+        tvSimpson.dataSource = self
     }
-
-
+    
+    func initSimpsonArray() {
+        simpsonArray = [homer, marge, bart, lisa, maggie, abraham, herbert, montgomery]
+    }
+    
+    func deleteSimpson(numberInArray: Int) {
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            deleteSimpson(numberInArray: indexPath.row)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = simpsonArray[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        simpsonArray.count
+    }
+    
 }
 
